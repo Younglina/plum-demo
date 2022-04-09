77e76dc3280060a1bf4318a6564954d178f3ac68
Younglina <Younglina0409@outlook.com>
2022-04-09 14:52:34 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+const el = $ref<HTMLCanvasElement>();
+
+const ctx = $computed(() => el!.getContext("2d")!);
+
+const WIDTH = 600;
+const HEIGHT = 600;
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+interface Branch {
+  start: Point;
+  length: number;
+  theta: number;
+}
+
+function init() {
+  ctx.strokeStyle = "#000";
+  step({
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 40,
+    theta: -Math.PI / 2,
+  });
+}
+
+const pendingTasks: Function[] = [];
+
+function step(b: Branch, depth = 0) {
+  const end = getEndPoint(b);
+  drawBranch(b);
+  if (depth < 4 || Math.random() < 0.5) {
+    pendingTasks.push(() =>
+      step(
+        {
+          start: end,
+          length: b.length + Math.random() * 10 - 5,
+          theta: b.theta - 0.3 * Math.random(),
+        },
+        depth + 1
+      )
+    );
+  }
+  if (depth < 4 || Math.random() < 0.5) {
+    pendingTasks.push(() =>
+      step(
+        {
+          start: end,
+          length: b.length + Math.random() * 10 - 5,
+          theta: b.theta + 0.3 * Math.random(),
+        },
+        depth + 1
+      )
+    );
+  }
+}
+let endFrame = false;
+function frame() {
+  if (pendingTasks.length === 0) {
+    endFrame = true;
+  }
+  const tasks = [...pendingTasks];
+  pendingTasks.length = 0;
+  tasks.forEach((fn) => fn());
+}
+
+let frameCount = 0;
+function startFrame() {
+  if (!endFrame) {
+    requestAnimationFrame(() => {
+      frameCount += 1;
+      if (frameCount % 3 === 0) frame();
+      startFrame();
+    });
+  }
+}
+
+startFrame();
+
+function lineTo(p1: Point, p2: Point) {
+  ctx.beginPath();
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
+  ctx.stroke();
+}
+
+function getEndPoint(b: Branch) {
+  return {
+    x: b.start.x + b.length * Math.cos(b.theta),
+    y: b.start.y + b.length * Math.sin(b.theta),
+  };
+}
+
+function drawBranch(b: Branch) {
+  lineTo(b.start, getEndPoint(b));
+}
+
+onMounted(() => {
+  init();
+});
+</script>
+
+<template>
+  <canvas ref="el" :width="WIDTH" :height="HEIGHT" border></canvas>
+</template>
